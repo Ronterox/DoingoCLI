94eaea37521e7e75a02e686f0c42810062b5758b
Ricardo <ricarditomontserrat@gmail.com>
2025-11-02 02:37:51 +0100

    Having all correct colors and same default recent functionality plus keywords

--- main.go
+++ main.go
@@ -22,14 +22,36 @@ func cit(text string, color string) string {
 	return "\033[" + color + "m" + text + "\033[0m"
 }
 
+func showRecent() {
+	file, err := os.Open(FILENAME)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return
+		}
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		split := strings.Split(line, "║")
+
+		date := split[0]
+		text := strings.Replace(split[1], "[Currently]", "", 1)
+
+		tabs := strings.Repeat(" ", max(88-len(date)-len(text), 0))
+		fmt.Println("\t"+cit(date, CYAN), "║", text, tabs, cit("[", PURPLE)+"Currently"+cit("]", PURPLE))
+	}
+}
+
 func main() {
 	if len(os.Args) > 1 {
-		fmt.Println(os.Args[1])
 		switch os.Args[1] {
 		case "now":
 			date := time.Now().Format("15:04am")
 			text := strings.Join(os.Args[2:], " ")
-			fmt.Println("New entry: added", date+": \"", text, "\" to Currently")
+			fmt.Println("\t", cit("New entry:", CYAN), "added", date+": \"", text, "\" to Currently")
 
 			file, err := os.OpenFile(FILENAME, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 			if err != nil {
@@ -37,29 +59,12 @@ func main() {
 			}
 			defer file.Close()
 			fmt.Fprintf(file, "%s ║ %s [Currently]\n", date, text)
+		case "recent":
+			showRecent()
 		default:
 			fmt.Println("Error: Command not found")
 		}
 	} else {
-		file, err := os.Open(FILENAME)
-		if err != nil {
-			if os.IsNotExist(err) {
-				return
-			}
-			fmt.Println(err)
-		}
-		defer file.Close()
-
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			line := scanner.Text()
-			split := strings.Split(line, "║")
-
-			date := split[0]
-			text := strings.Replace(split[1], "[Currently]", "", 1)
-
-			tabs := strings.Repeat("\t", max(22-len(date)-len(text)/3, 4))
-			fmt.Println("\t"+cit(date, CYAN), "║", text, tabs, cit("[", PURPLE)+"Currently"+cit("]", PURPLE))
-		}
+		showRecent()
 	}
 }
